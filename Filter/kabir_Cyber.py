cf25f7432efdff98e195dbdc180da385a9e6d65a
mamad010 <163658141+mamad010@users.noreply.github.com>
2024-03-16 21:11:24 +0330

    Create kabir_Cyber.py

---
+++ Filter/kabir_Cyber.py
@@ -0,0 +1,77 @@
+from colorama import Fore,init
+init()
+print(Fore.BLUE+"  MAMAD K꯭‌ ‌A꯭‌ ‌b꯭‌ ‌i꯭‌ ‌r꯭‌ ")
+
+print(Fore.GREEN+"""
+
+⠀print(Fore.RED+'''
+    oooo    oooo       .o.        .o8        o8o
+    888   .8P'       .888.      "888        "'
+     888  d8'        .8"888.      888oooo.  oooo  oooo d8b
+     88888[         .8' 888.     d88' 88b 888  888""8P
+     88888b.      .88ooo8888.    888   888  888   888
+     888  88b.   .8'     888.   888   888  888   888
+    o888o  o888o o88o     o8888o  Y8bod8P' o888o d888b''')
+""")
+
+
+
+print(Fore.RED+"""
+
+▄          ▄
+                          █▄        ██
+                          ███     ▄███
+                          ████████████
+                          ████████████
+                         ▄████████████
+                        ▄██▀███████▀███
+                        ███░▄██████░▄██▄
+                       ▄████████████████
+                       █████████████████
+                       █████████████████
+                    ▄                     ▄▄
+                   ███░█████████████████░▄███
+                  ████░█████████████████░████
+                  ████░█████████████████░████                                       ████░█████████████████░████
+                  ████░█████████████████░████
+                  ████░█████████████████░████
+                  ████░█████████████████░████
+                  ████░█████████████████░████
+                  ████░█████████████████░████
+                  ████░█████████████████░████
+                   ███░█████████████████░▀███
+                    ▀  █████████████████  ▀▀
+                       █████████████████
+                       ▀████████████████
+                        ▀▀▀███▀▀▀████▀▀
+                           ███   ████
+                           ███   ████
+                           ███   ████
+                           ███   ████
+                           ███   ████
+                           ▀█▀    ██
+
+""")
+from colorama import Fore,init
+init()
+print(Fore.RED)
+print('       ›››Cyber_Master_Cyber :‹‹‹ ' )
+print('')
+from colorama import Fore,init
+init()
+print(Fore.GREEN)
+NAME =input('   filtering RUBIKA :›››1 ‹‹‹ : ' )
+print('')
+print('http://dds6qkxpwdeubwucdiaord2xgbbeyds25rbsgr73tbfpqpt4a6vjwsyd.onion/http://cathug2kyi4ilneggumrenayhuhsvrgn6qv2y47bgeet42iivkpynqad.onion/')
+print('')
+NAME =input('   filtering RUBIKA :›››2 ‹‹‹ : ' )
+print('')
+print('https://www.uplooder.net/img/image/45/7aa3c4d6a5d5b09a0b4942b08effd0b6/IMG-20230517-232423-114.jpg')
+print('')
+NAME =input('   filtering RUBIKA :›››3‹‹‹ : ' )
+print('')
+print('http://Report.rubika.ir/Infractions:child-abuse.Anti-Islamic-rebellion.Anti-Islam-and-leader.Phishing-Rubika.SupportBot')
+print('')
+NAME =input('   filtering RUBIKA :›››4‹‹‹ : ' )
+print('')
+print('https://cosmosmagazine.com/people/society/dark-web-weapons-trade-could-grow-travel-and-change-very-quickly/#(*code.server.hack=tor.1.0.0.127:9050.(3.5.3.6)./rest_122334_filteri.com/y/d/f.310.256.5.106.10.266.Fil)(https://reliefweb.int/report/world/global-terrorism-index-2023)(*http://6.0.3.6/d///f//i///y.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4.6.0.3.6.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4)(https://гомосексуальность.с.представителями.своего.пола.и.отправка.сексуальных.и.грубых.видео.пользователям.Rubika.заблокируйте.этого.пользователя)(*http://dxprit-L-f-c-sait-rubika-Netreport.gigfa.com)(http://Filter.account.rubika.server.offline.onlline.in.sheytan.cyber.py)(*https://github.com/Rubika-hacker/http-dxprit-saleh-king99-55-39-hackers.phpnet.us-dxprit.html)(https://github.com/Rubika-hacker/http-dxprit-saleh-king99-55-39-hackers.phpnet.us-dxprit.html)(*https://www.c4isrnet.com/industry/2018/09/25/why-the-market-for-zero-day-vulnerabilities-on-the-dark-web-is-vanishing/)(https://www.nytimes.com/2020/03/04/technology/can-you-hire-a-hit-man-online.html)(https://cosmosmagazine.com/people/society/dark-web-weapons-trade-could-grow-travel-and-change-very-quickly/)(https://s2.uupload.ir/files/inshot_20230131_145402904_rerk.jpg)(*http://sexy.movie.com)(*http://xxx.com)(*http://pornhub.com*')
